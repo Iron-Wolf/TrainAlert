f9de584c5699fdd231d4e4db139812baac5e1038
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2016-11-30 22:17:12 +0100

    fix notification

--- src/ressources/CronJob.java
+++ src/ressources/CronJob.java
@@ -39,30 +39,43 @@ public class CronJob extends TelegramLongPollingBot implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         StringBuilder messageBuilder = new StringBuilder(Emoji.ALARM_CLOCK + " Alerte automatique : \n");
-        String message;
+        boolean sendMessage = false;
 
         try {
             // SNCF transilien
             String fullSNCFUrl = String.format(BotConfig.SNCF_URL, BotConfig.COLOMBES, BotConfig.ST_LAZARD);
             InputStream content = APIWorker.getInstance().getXMLData(fullSNCFUrl, BotConfig.SNCF_USER, BotConfig.SNCF_PASSWD);
-            message = "Départ Colombes -> " + APIWorker.getInstance().getMessageJLine(content);
+            String messageColombes = APIWorker.getInstance().getMessageJLine(content);
 
             fullSNCFUrl = String.format(BotConfig.SNCF_URL, BotConfig.ST_LAZARD, BotConfig.COLOMBES);
             content = APIWorker.getInstance().getXMLData(fullSNCFUrl, BotConfig.SNCF_USER, BotConfig.SNCF_PASSWD);
-            message += "Départ St Lazard -> " + APIWorker.getInstance().getMessageJLine(content);
+            String messageParis = APIWorker.getInstance().getMessageJLine(content);
 
             // RATP subway
+            String messageRatp = null;
             for (Integer subwayLine : BotConfig.SUBWAY_LIST) {
                 String fullRATPUrl = String.format(BotConfig.RATP_SUBWAY_URL, subwayLine.toString());
-                InputStream ratpContent =  APIWorker.getInstance().getXMLData(fullRATPUrl);
-                message +=  APIWorker.getInstance().getMessageSubway(ratpContent, false);
+                InputStream ratpContent = APIWorker.getInstance().getXMLData(fullRATPUrl);
+                messageRatp = APIWorker.getInstance().getMessageSubway(ratpContent, false);
+            }
+
+            // append text to the message
+            if (!messageColombes.isEmpty()) {
+                messageBuilder.append("Départ Colombes - " + messageColombes);
+                sendMessage = true;
+            }
+            if (!messageParis.isEmpty()) {
+                messageBuilder.append("Départ Paris - " + messageParis);
+                sendMessage = true;
+            }
+            if (!messageRatp.isEmpty()) {
+                messageBuilder.append(messageRatp);
+                sendMessage = true;
             }
 
             // send message only if something is returned from the APIWorker
-            if (!message.isEmpty()) {
-                messageBuilder.append(message);
+            if (sendMessage)
                 sendAlerts(messageBuilder);
-            }
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             BotLogger.error(LOGTAG, e);
